16ed2666686b2b483b0afb114903f83d670c29a3
DIPJOY PAUL ANTIK <antikpaul1994@gmail.com>
2022-05-02 21:18:40 +0600

    Using indexing useState()

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,18 +1,23 @@
 import React, { useState } from "react";
 
 const Test = (props) => {
-  const testState = useState("");
+  const myName = useState("");
 
   //*   console.log(testState[0]);
   //*  console.log(testState[1]);
 
-  if (testState[0] === ""){
-    testState[1]("Hello World!");
+  // if (testState[0] === ""){
+  //   testState[1]("Hello World!");
+  // }
+
+  const changeState = ()=>{
+    myName[1]("DIPJOY");
   }
 
   return (
     <div>
-      <h1>{testState[0]}</h1>
+      <button onClick={changeState}>Show</button>
+      <h1>{myName[0]}</h1>
     </div>
   );
 };
